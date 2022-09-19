c2ee2ba1125c2be28879bde6d41998b599c04a00
hdubey64 <hdubey64@gmail.com>
2022-09-19 20:22:47 +0530

    Infut Css Fixed

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let firstNumber = document.getElementById(num1);
+let secondNumber = document.getElementById(num2);
+let calculat = document.getElementById(operator);
+function result() {
+    return ${firstNumber} ${calculat} ${secondNumber}
+}
+
+result():
