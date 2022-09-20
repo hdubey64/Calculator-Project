9ab35bc22f23d4a7a3a81f7185e99595e707e5e5
hdubey64 <hdubey64@gmail.com>
2022-09-20 11:03:51 +0530

    All Operater working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,59 @@
-let firstNumber = document.getElementById(num1);
-let secondNumber = document.getElementById(num2);
-let calculat = document.getElementById(operator);
-function result() {
-    return ${firstNumber} ${calculat} ${secondNumber}
+// let firstNumber = 10;
+// let secondNumber = 20;
+// let calculat = "+";
+// function myCalculater(firstNumber, calculat, secondNumber) {
+//    let result = `${firstNumber} ${calculat} ${secondNumber}`;
+//    return result;
+// }
+
+// myCalculater();
+
+function add() {
+   let firstNumber = document.querySelector("#num1").value;
+   let secondNumber = document.querySelector("#num2").value;
+
+   let result = document.getElementById("result");
+
+   result.innerHTML = parseInt(firstNumber) + parseInt(secondNumber);
+}
+function minus() {
+   let firstNumber = document.querySelector("#num1").value;
+   let secondNumber = document.querySelector("#num2").value;
+
+   let result = document.getElementById("result");
+
+   result.innerHTML = parseInt(firstNumber) - parseInt(secondNumber);
+   console.log(firstNumber);
+}
+function multiply() {
+   let firstNumber = document.querySelector("#num1").value;
+   let secondNumber = document.querySelector("#num2").value;
+
+   let result = document.getElementById("result");
+
+   result.innerHTML = `Your Result Is :- ${
+      parseInt(firstNumber) * parseInt(secondNumber)
+   }`;
+}
+function divide() {
+   let firstNumber = document.querySelector("#num1").value;
+   let secondNumber = document.querySelector("#num2").value;
+
+   let result = document.getElementById("result");
+
+   result.innerHTML = `Your Result Is :- ${(
+      parseInt(firstNumber) / parseInt(secondNumber)
+   ).toFixed(2)}`;
 }
 
-result():
+function remainder() {
+   let firstNumber = document.querySelector("#num1").value;
+   let secondNumber = document.querySelector("#num2").value;
+
+   let result = document.getElementById("result");
+
+   result.innerHTML = `Your Result is : ${(
+      (parseInt(firstNumber) / parseInt(secondNumber)) *
+      100
+   ).toFixed(2)}%`;
+}
